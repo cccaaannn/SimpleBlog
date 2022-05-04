94ac64cbce83cd360953a4be41651655761903ff
cccaaannn <can.kurt.aa@gmail.com>
2022-05-04 19:33:57 +0300

    misc

--- src/core/middlewares/exception-handler.middleware.ts
+++ src/core/middlewares/exception-handler.middleware.ts
@@ -4,7 +4,7 @@ import { ErrorResult } from "../results/Result";
 function exceptionHandler() {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
-            // console.error(`Error ${res.locals.err.message}`);
+            console.error(`Error ${res.locals.err.message}`);
             return res.status(500).json(new ErrorResult("Internal server error"));
         } catch (err: any) {
             next(err);
