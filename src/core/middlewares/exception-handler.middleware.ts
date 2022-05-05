e7961fd08f6f46ad7159e197d85f4c6284fb583b
cccaaannn <can.kurt.aa@gmail.com>
2022-05-05 19:12:10 +0300

    bug fix

--- src/core/middlewares/exception-handler.middleware.ts
+++ src/core/middlewares/exception-handler.middleware.ts
@@ -4,7 +4,6 @@ import { ErrorResult } from "../results/Result";
 function exceptionHandler() {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
-            console.error(`Error ${res.locals.err.message}`);
             return res.status(500).json(new ErrorResult("Internal server error"));
         } catch (err: any) {
             next(err);
