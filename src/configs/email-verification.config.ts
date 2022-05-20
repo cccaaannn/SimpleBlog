0fecc940808a3406bac4db4b355569388f4b1bad
cccaaannn <can.kurt.aa@gmail.com>
2022-05-20 21:08:18 +0300

    email verification templates changed

--- src/configs/email-verification.config.ts
+++ src/configs/email-verification.config.ts
@@ -1,15 +1,15 @@
 export const EmailAuthConfig = {
 
     AccountVerification: {
-        EMAIL_SUBJECT: "Account activation",
-        EMAIL_TEXT: "It appears your email client does not supports html content, please visit the link to verify your account.\n{{URL}}",
+        EMAIL_SUBJECT: "Account activation - Simple Blog",
+        EMAIL_TEXT: "Hi {{USERNAME}}, it appears your email client does not supports html content, please visit the link to activate your account.\n{{URL}}",
         EMAIL_HTML_TEMPLATE_PATH: 'src/resources/account-verification-email-template.html',
         JWT_TOKEN_EXPIRATION: "3600s" // 1 hour
     },
 
     PasswordReset: {
-        EMAIL_SUBJECT: "Password reset",
-        EMAIL_TEXT: "It appears your email client does not supports html content, please visit the link to reset your password.\n{{URL}}",
+        EMAIL_SUBJECT: "Password reset - Simple Blog",
+        EMAIL_TEXT: "Hi {{USERNAME}}, it appears your email client does not supports html content, please visit the link to reset your password.\n{{URL}}",
         EMAIL_HTML_TEMPLATE_PATH: 'src/resources/password-reset-email-template.html',
         JWT_TOKEN_EXPIRATION: "3600s" // 1 hour
     }
