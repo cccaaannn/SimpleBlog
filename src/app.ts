4a551d99bad57ac7d438606d1f89ce07070abdca
cccaaannn <can.kurt.aa@gmail.com>
2022-06-07 21:56:25 +0300

    log skip

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ app.use(cors());
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(morgan('common', { stream: fs.createWriteStream(path.join(__dirname, '../logs/access.log'), { flags: 'a' }) }))
+app.use(morgan('common', { stream: fs.createWriteStream(path.join(__dirname, '../logs/access.log'), { flags: 'a' }), skip: (req, res) => process.env.NODE_ENV === 'test' }))
 
 
 app.use('/api/v1/users', extractTokenIfExists(), decodeAndVerifyToken(), UsersRouter);
