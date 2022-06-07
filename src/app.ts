1b1f98b595f0843a69da28bad772d7bc8eb7de64
cccaaannn <can.kurt.aa@gmail.com>
2022-06-07 21:36:27 +0300

    access log added

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,24 @@
+// Node
+import path from "path";
+import fs from "fs";
+
 // Main lib imports
 import express from "express";
 
 // Side lib imports
 import dotenv from "dotenv";
 dotenv.config();
-import cors from 'cors';
-import helmet from 'helmet';
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
 
 // Project imports
 import { extractTokenIfExists, decodeAndVerifyToken } from "./core/middlewares/secured-operation.middleware";
+import { exceptionHandler } from "./core/middlewares/exception-handler.middleware";
+import { notFound } from "./core/middlewares/not-found.middleware";
 import UsersRouter from "./routes/api/v1/users.routes";
 import PostRouter from "./routes/api/v1/post.routes";
 import AuthRouter from "./routes/api/v1/auth.routes";
-import { exceptionHandler } from "./core/middlewares/exception-handler.middleware";
-import { notFound } from "./core/middlewares/not-found.middleware";
 
 
 const app = express();
@@ -22,6 +27,7 @@ app.use(cors());
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(morgan('common', { stream: fs.createWriteStream(path.join(__dirname, '../logs/access.log'), { flags: 'a' }) }))
 
 
 app.use('/api/v1/users', extractTokenIfExists(), decodeAndVerifyToken(), UsersRouter);
