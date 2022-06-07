1b3f64051373c89f9cdb7c6eb81990339fa0f3d6
cccaaannn <can.kurt.aa@gmail.com>
2022-06-07 22:02:24 +0300

    log fix

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,9 @@ app.use(cors());
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(morgan('common', { stream: fs.createWriteStream(path.join(__dirname, '../logs/access.log'), { flags: 'a' }), skip: (req, res) => process.env.NODE_ENV === 'test' }))
+if(process.env.NODE_ENV !== 'test') {
+    app.use(morgan('common', { stream: fs.createWriteStream(path.join(__dirname, '../logs/access.log'), { flags: 'a' }) }))
+}
 
 
 app.use('/api/v1/users', extractTokenIfExists(), decodeAndVerifyToken(), UsersRouter);
